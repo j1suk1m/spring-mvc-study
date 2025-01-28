890fb32d4afe0da5780702b2ec432a2fd30c5ca6
jisukimtech <jisukim.tech@gmail.com>
2025-01-29 04:01:03 +0900

    feat: [section 3] HTTP 응답 데이터 - API JSON

---
+++ 1편 - 백엔드 웹 개발 핵심 기술/jsp/src/main/java/study/servlet/basic/response/ResponseJsonServlet.java
@@ -0,0 +1,29 @@
+package study.servlet.basic.response;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import study.servlet.basic.HelloData;
+
+@WebServlet(name = "responseJsonServlet", urlPatterns = "/response-json")
+public class ResponseJsonServlet extends HttpServlet {
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+
+        HelloData helloData = new HelloData();
+        helloData.setName("hello");
+        helloData.setAge(20);
+
+        String result = objectMapper.writeValueAsString(helloData); // HelloData 객체를 JSON 데이터로 변환
+        response.getWriter().write(result);
+    }
+
+}
