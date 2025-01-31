19ced8cabf980111008be6775bf796cda0173ae0
jisukimtech <jisukim.tech@gmail.com>
2025-02-01 06:15:08 +0900

    feat: [section 6] 스프링 MVC - 시작하기

---
+++ 1편 - 백엔드 웹 개발 핵심 기술/mvc/src/main/java/study/servlet/web/springmvc/v1/SpringMemberSaveControllerV1.java
@@ -0,0 +1,30 @@
+package study.servlet.web.springmvc.v1;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import study.servlet.domain.member.Member;
+import study.servlet.domain.member.MemberRepository;
+
+@Controller
+public class SpringMemberSaveControllerV1 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/springmvc/v1/members/save")
+    public ModelAndView process(HttpServletRequest request, HttpServletResponse response) {
+        String name = request.getParameter("name");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(name, age);
+        memberRepository.save(member);
+
+        ModelAndView modelAndView = new ModelAndView("save-result");
+        modelAndView.addObject("member", member);
+
+        return modelAndView;
+    }
+
+}
