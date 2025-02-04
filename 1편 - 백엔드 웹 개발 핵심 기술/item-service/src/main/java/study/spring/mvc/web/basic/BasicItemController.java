288d4b3a37b1a10b922efa00cc5e8b8172916ac5
jisukimtech <jisukim.tech@gmail.com>
2025-02-04 15:47:20 +0900

    feat: [section 8] 상품 등록 처리 - `@`ModelAttribute

--- 1편 - 백엔드 웹 개발 핵심 기술/item-service/src/main/java/study/spring/mvc/web/basic/BasicItemController.java
+++ 1편 - 백엔드 웹 개발 핵심 기술/item-service/src/main/java/study/spring/mvc/web/basic/BasicItemController.java
@@ -6,9 +6,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import study.spring.mvc.domain.item.Item;
 import study.spring.mvc.domain.item.ItemRepository;
 
@@ -37,9 +39,53 @@ public class BasicItemController {
         return "basic/addForm";
     }
 
+    // @PostMapping("/add")
+    public String addItemV1(
+            @RequestParam String itemName,
+            @RequestParam int price,
+            @RequestParam Integer quantity,
+            Model model
+    ) {
+        Item item = new Item(itemName, price, quantity);
+        itemRepository.save(item);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+    /**
+     * @ModelAttribute의 기능
+     * 1. 객체를 생성한 후 프로퍼티 접근법으로 요청 파라미터의 값 설정
+     * 2. 지정한 name을 이용해 Model에 저장
+     * @param item
+     * @return viewName
+     */
+    // @PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item) {
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
+    /**
+     * @ModelAttribute name 생략 가능
+     * Model에 저장되는 name은 클래스 이름의 카멜케이스 버전
+     * @param item
+     * @return viewName
+     */
+    // @PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item) {
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
+    /**
+     * @ModelAttribute 자체 생략 가능
+     * @param item
+     * @return viewName
+     */
     @PostMapping("/add")
-    public String addItem() {
-        return "xxxxx";
+    public String addItemV4(Item item) {
+        itemRepository.save(item);
+        return "basic/item";
     }
 
     /**
