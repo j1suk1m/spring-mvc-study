b222acc8f0393f5d51e75e98d073f673bc8ef7bd
jisukimtech <jisukim.tech@gmail.com>
2025-02-17 15:21:09 +0900

    feat: [section 2] 기본 객체들

--- 2편 - 백엔드 웹 개발 활용 기술/thymeleaf-basic/src/main/java/study/spring/thymeleaf/basic/BasicController.java
+++ 2편 - 백엔드 웹 개발 활용 기술/thymeleaf-basic/src/main/java/study/spring/thymeleaf/basic/BasicController.java
@@ -1,11 +1,15 @@
 package study.spring.thymeleaf.basic;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -45,11 +49,34 @@ public class BasicController {
         return "basic/variable";
     }
 
+    @GetMapping("basic-objects")
+    public String renderBasicObjectsView(
+            Model model,
+            HttpServletRequest request,
+            HttpServletResponse response,
+            HttpSession session
+    ) {
+        session.setAttribute("sessionData", "Hello Session");
+
+        model.addAttribute("request", request);
+        model.addAttribute("response", response);
+        model.addAttribute("servletContext", request.getServletContext());
+
+        return "basic/basic-objects";
+    }
+
     @Data
     @AllArgsConstructor
     static class User {
         private String username;
         private int age;
+    }
+
+    @Component("helloBean")
+    static class HelloBean {
+        public String hello(String data) {
+            return "Hello " + data;
+        }
     }
 
 }
